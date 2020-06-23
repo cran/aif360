0a5136a82bea3673ea4816387d3e1f7b376eea8b
Saishruthi Swaminathan <saishruthi.tn@ibm.com>
2020-06-23 13:20:02 +0000

    version 0.1.0

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(aif360)
+
+test_check("aif360")
---
+++ tests/testthat/test-1.R
@@ -0,0 +1,8 @@
+# load aif360 functions
+test_that("Things work as expected", {
+  skip_on_cran()
+  # load aif library
+  load_aif360_lib()
+})
+
+
